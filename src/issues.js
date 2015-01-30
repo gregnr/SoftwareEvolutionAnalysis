9424ace60852cd807034342e8d064f900c7e272f
Jordan Heemskerk <jheems@c89.seng.uvic.ca>
2015-01-29 16:14:44 -0800

    Basic async working, need to work on paging

--- src/issues.js
+++ src/issues.js
@@ -11,38 +11,36 @@ var count_up = 0;
 var i = 0;
 var reqs = [];
 for (i = 0; i < count; i++) {
-	console.log("run " + i);
-	runRequest(i);
+	reqs.push(function (callback) {runRequest(callback, k=i);});
 }
 
-while (count_up < count) ;
+async.parallel(reqs, function (err, result) {
+    process.stdout.write(result);
+});
 
 process.stdout.write(all_json);
 
-function runRequest (callback, i) {
+function runRequest (callback, k) {
+	var t = k;
 	var options = {
 		host: 'api.github.com',
-		path: '/repos/jquery/jquery/issues?access_token=ce72f33fa9f02b9a2ecb60b983e6cfbd9dedd4ed&status=all&per_page=100&page=' + i,
+		path: '/repos/jquery/jquery/issues?access_token=db9842477822fb8a9a0aa330a846dcb5f656f9df=&status=all&per_page=100&page=' + t,
 		headers: {'user-agent': 'jordan-heemskerk'},
 	};
 
 	console.log("send");
-	https.get(options, function(res) {
+	var string = "";
+	console.log(options.path);
+	https.get(options, function(response) {
 		//console.log("statusCode: ", res.statusCode);
 		//console.log("headers: ", res.headers);
 
-		//handle async response
-		res.on('data', function(d) {
-			//successful request
-			process.stdout.write("Response " + i);
-			//process.stdout.write(d);
-
-			all_json += d;
-			count_up++;
-			console.log("Progress: " + (count_up/count) * 100 + "%");
-		});
-
-		
+      response.on('data', function(response){
+	      string += response;
+	  }); 
+	    response.on('end', function(){
+	      console.log(string);
+	    });  	
 
 	}).on('error', function(e) {
 		//handle error
