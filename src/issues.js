9b2e8fe854fa103d75975502d1113afdc9db2ff0
Jordan Heemskerk <jordanh@shaw.ca>
2015-01-30 17:48:19 -0800

    Implement #2
        Had to end up using doWhilst, so our https calls are no
        longer parallel but are in series. This was to simplfy
        the code that determines how many pages we need to
        grab.
    
        We just need to wrap this functionality into a class now.

--- src/issues.js
+++ src/issues.js
@@ -3,17 +3,17 @@ var https = require("https");
 var async = require("async");
 var prompt = require("prompt");
 
-var DEBUG = false;
-
 //Initialize variables
 var gUsername;
 var gPassword;
 var gUrl;
+var giLastPage = 0;
+var gIssues = [];
 
 //Utility functions
 var getBasicAuthenticationHeader = function(username, password) {
 
-	return "Basic " + new Buffer(username + ":" + password).toString("base64");
+    return "Basic " + new Buffer(username + ":" + password).toString("base64");
 };
 
 var getGithubPathFromUrl = function(url, page_num, issue_status, per_page) {
@@ -30,10 +30,8 @@ var getGithubPathFromUrl = function(url, page_num, issue_status, per_page) {
     if (match !== null) {
         var path = match.slice(1);
         
-        var toReturn = "/repos/" + path + "/issues?status=" + issue_status +
+        var toReturn = "/repos/" + path + "/issues?state=" + issue_status +
             "&per_page=" + per_page + "&page=" + page_num; 
-
-        if (DEBUG) console.log("Generated path: " + toReturn);
 
         return toReturn;
 
@@ -44,114 +42,140 @@ var getGithubPathFromUrl = function(url, page_num, issue_status, per_page) {
  
 }
 
+var getLastPageFromHeaders = function (headers) {
+
+    var sLastPage = (/\, <.*page=(.+).*>; rel="last"/).exec(headers["link"]).slice(1);
+    
+    return parseInt(sLastPage);
+
+}
 
 //Async functions
 var promptCredentials = function(callback) {
 
-	var properties = [
-		{
-			name: "username", 
-			validator: /^[a-zA-Z\s\-]+$/,
-			warning: "Username must be only letters, spaces, or dashes"
-		},
-		{
-			name: "password",
-			hidden: true
-		},
+    var properties = [
+        {
+            name: "username", 
+            validator: /^[a-zA-Z\s\-]+$/,
+            warning: "Username must be only letters, spaces, or dashes"
+        },
+        {
+            name: "password",
+            hidden: true
+        },
         {
             name: "url",
             validator: /https?:\/\/github.com\/*/,
             warning: "URL must be to a GitHub repository"
         }
-	];
-	
-	//Init prompt
-	prompt.start();
+    ];
+    
+    //Init prompt
+    prompt.start();
 
-	//Prompt for Github username and password
-	prompt.get(properties, function (err, result) {
+    //Prompt for Github username and password
+    prompt.get(properties, function (err, result) {
 
-		if (err) {
-			console.error(err);
-			return;
-		}
-		
-		gUsername = result.username;
-		gPassword = result.password;
+        if (err) {
+            console.error(err);
+            return;
+        }
+        
+        gUsername = result.username;
+        gPassword = result.password;
         gUrl = result.url;
-		
-		callback();
-	});
+        
+        callback();
+    });
 };
 
 var issueRequest = function(callback, page_num) {
 
-	var options = {
-		host: "api.github.com",
-		path: getGithubPathFromUrl(gUrl, page_num),
-		headers: {
-			"user-agent": "UnitTestBugAnalyzer",
-			"Authorization": getBasicAuthenticationHeader(gUsername, gPassword)
-		},
-	};
+    var options = {
+        host: "api.github.com",
+        path: getGithubPathFromUrl(gUrl, page_num),
+        headers: {
+            "user-agent": "UnitTestBugAnalyzer",
+            "Authorization": getBasicAuthenticationHeader(gUsername, gPassword)
+        },
+    };
 
-	var resp = "";
+    // update user of progress
+    if (giLastPage != 0) {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write("sending issueRequest for page " + page_num + " of " + giLastPage + " (" + Math.round((page_num/giLastPage) * 100) + "%)");    
+    } else  {
+        //we don't know how many pages if giLastPage not updated
+        process.stdout.write("sending issueRequest for page " + page_num + " of ?");    
+    }
 
-    console.log("sending issueRequest for page " + page_num + " of 10");	
+    https.get(options, function(response) {
 
-	https.get(options, function(response) {
+        giLastPage = getLastPageFromHeaders(response.headers);
 
-        if (DEBUG) {
-            console.log("statusCode: ", response.statusCode);
-            console.log("headers: ", response.headers);
-        }       
+        var data = ""; 
+        response.on("data", function(streaming_resp){
 
-		response.on("data", function(response){
-			resp += response;
-		}); 
-		
-		response.on("end", function(){
-			callback(null, resp);
-		});
+            //collect stream
+            data += streaming_resp;
 
-	}).on("error", function(e) {
-		callback(e);
-	});
+        }); 
+        
+        response.on("end", function(){
+
+            //push data on to global gIssues array
+            gIssues.push(JSON.parse(data));
+
+            //done this request
+            callback(null);
+        });
+
+    }).on("error", function(e) {
+        callback(e);
+    });
 }
 
 var fetchIssues = function(callback) {
-    if (DEBUG) console.log("issues.js:fetchIssues+");
-	var reqs = [];
+    
+    //pages are 1-indexed
+    var page = 1;
+    
+    async.doWhilst(
+        
+        //repeating function call
+        function (callback) {
+            (function(page) {
+                issueRequest(callback, page);
+            })(page);
+            page++;
+        },
+        
+        // async.doWhilst stops itteration when this function returns false
+        function() { 
+            //Note that giLastPage is dynamically updated based on request results
+            return page < giLastPage;  
+        },
 
-    //TODO: implement proper pagination
-    //Note: pages are 1-indexed
-	for (var i = 1; i < 10; i++) {
+        //called after itteration stops
+        function (err) { 
 
-		(function(i) {
-	
-			reqs.push(function (callback) {
-		
-				issueRequest(callback, i);
-			})
-		})(i);
-	}
+            //write out a \n when done with updating user
+            process.stdout.write("\n"); 
+            
+            //flatten array
+            var flatIssues = [];
+            flatIssues = flatIssues.concat.apply(flatIssues, gIssues);
 
+            //just write the number a title of issues to stdout for now            
+            for (var j = 0; j < flatIssues.length; j++) {
+                var issue = flatIssues[j];
+                console.log(issue.number + ": " + issue.title);
+            } 
+            callback();
+        }
+    );
 
-	//Run all web API requests in parallel
-	async.series(reqs, function (err, result) {
-	
-		if (err) {
-			console.error(err);
-			return;
-		}
-		
-        console.log("Done grabbing issues");
-		if (DEBUG) console.log(result);
-		
-		callback();
-	});
-
-    if (DEBUG) console.log("issues.js:fetchIssues-");
 };
 
 //Run async functions sequentially
