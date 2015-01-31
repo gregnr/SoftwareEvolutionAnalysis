8641a2e34544b89eb27c6d5b5370ce8ae6e63e2b
Jordan Heemskerk <jordanh@shaw.ca>
2015-01-30 16:03:55 -0800

    Address #2, allow user to enter url.
        Add debugging outputs. Also, formatting changes, etc.

--- src/issues.js
+++ src/issues.js
@@ -3,15 +3,46 @@ var https = require("https");
 var async = require("async");
 var prompt = require("prompt");
 
+var DEBUG = false;
+
 //Initialize variables
-var username;
-var password;
+var gUsername;
+var gPassword;
+var gUrl;
 
 //Utility functions
 var getBasicAuthenticationHeader = function(username, password) {
 
 	return "Basic " + new Buffer(username + ":" + password).toString("base64");
 };
+
+var getGithubPathFromUrl = function(url, page_num, issue_status, per_page) {
+    
+    //assign default values
+    issue_status = typeof issue_status !== 'undefined' ? issue_status : "all";
+    per_page = typeof per_page !== 'undefined' ? per_page : 100;
+
+    //Regex to extract path
+    path_pattern = /\/\/[^\/]*\/(.*)$/; 
+
+    var match = path_pattern.exec(url);
+    
+    if (match !== null) {
+        var path = match.slice(1);
+        
+        var toReturn = "/repos/" + path + "/issues?status=" + issue_status +
+            "&per_page=" + per_page + "&page=" + page_num; 
+
+        if (DEBUG) console.log("Generated path: " + toReturn);
+
+        return toReturn;
+
+    } else {
+        console.error("Could not get path from url: " + url);
+        return;
+    }
+ 
+}
 
 
 //Async functions
@@ -26,7 +57,12 @@ var promptCredentials = function(callback) {
 		{
 			name: "password",
 			hidden: true
-		}
+		},
+        {
+            name: "url",
+            validator: /https?:\/\/github.com\/*/,
+            warning: "URL must be to a GitHub repository"
+        }
 	];
 	
 	//Init prompt
@@ -40,31 +76,35 @@ var promptCredentials = function(callback) {
 			return;
 		}
 		
-		username = result.username;
-		password = result.password;
+		gUsername = result.username;
+		gPassword = result.password;
+        gUrl = result.url;
 		
 		callback();
 	});
 };
 
-var issueRequest = function(callback, i) {
+var issueRequest = function(callback, page_num) {
 
 	var options = {
 		host: "api.github.com",
-		path: "/repos/jquery/jquery/issues?status=all&per_page=100&page=" + i,
+		path: getGithubPathFromUrl(gUrl, page_num),
 		headers: {
 			"user-agent": "UnitTestBugAnalyzer",
-			"Authorization": getBasicAuthenticationHeader(username, password)
+			"Authorization": getBasicAuthenticationHeader(gUsername, gPassword)
 		},
 	};
 
 	var resp = "";
-	console.log(options.path);
-	
+
+    console.log("sending issueRequest for page " + page_num + " of 10");	
+
 	https.get(options, function(response) {
 
-		//console.log("statusCode: ", res.statusCode);
-		//console.log("headers: ", res.headers);
+        if (DEBUG) {
+            console.log("statusCode: ", response.statusCode);
+            console.log("headers: ", response.headers);
+        }       
 
 		response.on("data", function(response){
 			resp += response;
@@ -80,10 +120,12 @@ var issueRequest = function(callback, i) {
 }
 
 var fetchIssues = function(callback) {
-
+    if (DEBUG) console.log("issues.js:fetchIssues+");
 	var reqs = [];
 
-	for (var i = 0; i < 50; i++) {
+    //TODO: implement proper pagination
+    //Note: pages are 1-indexed
+	for (var i = 1; i < 10; i++) {
 
 		(function(i) {
 	
@@ -94,18 +136,22 @@ var fetchIssues = function(callback) {
 		})(i);
 	}
 
+
 	//Run all web API requests in parallel
-	async.parallel(reqs, function (err, result) {
+	async.series(reqs, function (err, result) {
 	
 		if (err) {
 			console.error(err);
 			return;
 		}
 		
-		console.log(result);
+        console.log("Done grabbing issues");
+		if (DEBUG) console.log(result);
 		
 		callback();
 	});
+
+    if (DEBUG) console.log("issues.js:fetchIssues-");
 };
 
 //Run async functions sequentially
