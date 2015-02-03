501052cf439d3cd6b59c516bb81cce7c82d54ac3
Jordan Heemskerk <jordanh@shaw.ca>
2015-02-02 17:43:58 -0800

    Use nodegit instead of gift
        Fixes: #8 and #11

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 var async = require("async");
-var gift = require("gift");
+var nodegit = require("nodegit")
 var prompt = require("prompt");
 var mkdirp = require("mkdirp");
 var yargs = require("yargs")
@@ -25,6 +25,7 @@ if (argv.help) {
 
 //Initialize globals
 var gIssues = [];
+var gCommits = [];
 var gUsername;
 var gPassword;
 var gUrl;
@@ -140,37 +141,93 @@ var printIssues = function(callback) {
         //console.log(issue.number + ": " + issue.title);
     }
 
+    for (var k = 0; k < gCommits.length; k++) {
+        var commit = gCommits[k];
+        console.log("commit: " + commit.sha() + " on " + commit.date());;
+    }
+
     return callback();
+}
+
+
+var scrapeCommits = function (firstCommitOnMaster, callback) {
+   
+    var history = firstCommitOnMaster.history();
+    
+    history.on("commit", function (commit) { 
+        gCommits.push(commit);
+    });
+    
+    history.on("end", function (commit) {
+        return callback();
+    });
+
+    history.start();
 }
 
 var loadRepoHistory = function (callback) {
    
     //parse string params
-    var checkout_url = gUrl + ".git";
-    var repo_name = (/^.*\/([^\/]+)\.git$/).exec(checkout_url).slice(1);
+    var repo_name = (/^.*\/([^\/]+)$/).exec(gUrl).slice(1);
     var repo_path = "./.repo_cache/" + repo_name;
     
     
     // create .repo_cache directory if it does not exist
-    mkdirp(repo_path, function(err) { 
+    mkdirp(repo_path, function(err, created_dir) { 
         
         if (err) {
             console.error(err);
             return;
         }
 
-        console.log("Cloning repository: " + repo_name);
-        
-        // clone repo to local filesystem using gift module
-        gift.clone(checkout_url, repo_path, 
+        //TODO: prompt whether to overwrite cache
+        if (created_dir == undefined) {
+
+            console.log("Syncing cached repository: " + repo_name);
             
-            function (err, result) {    
-                
-                callback(); 
+            var repository;
 
-            }
-        );
+            nodegit.Repository.open(repo_path)
 
+                .then(function (repo) {
+                    repository = repo;
+                    return repository.fetchAll({}, true);
+                }, function (err) { console.error(err); return;})
+
+                .then(function () {
+                    return repository.mergeBranches("master", "origin/master");
+                }, function (err) {console.error(err); return;})
+
+                .then(function () {
+                    return repository.getMasterCommit();
+                })
+
+                .then(function (firstCommitOnMaster) {
+                    scrapeCommits(firstCommitOnMaster, callback);
+                });
+
+
+        } else {
+
+            console.log("Cloning repository: " + repo_name);     
+            
+            // clone repo to local filesystem using nodegit module
+            // ignore https cert errors due to https://github.com/nodegit/nodegit/issues/322
+            nodegit.Clone.clone(gUrl, repo_path, { ignoreCertErrors: 1})
+
+                .then(function (repo) {
+                    return repo.getMasterCommit();
+                }, function (err) { console.error(err); return;})
+
+                .then(function (firstCommitOnMaster) {
+                    scrapeCommits(firstCommitOnMaster, callback); 
+                });
+
+        }
+
+        
+            
+            
     }); 
 
 
