8459a3c8b1419f32c296f176544ad23d5a0e9cf8
Parker Atkins <parker.cattell.atkins@gmail.com>
2015-02-25 14:01:37 -0800

    Formatted plotly graph

--- src/graph.js
+++ src/graph.js
@@ -3,15 +3,22 @@ plotly = plotly("seng371", "k66ptaq4eh");
 
 module.exports.graph_me  = function (repo, data, callback) {
 	
-	var layout = {autosize: false,
-			width: 500,
-			length: 500,
+	var layout = {title: "Change in Test Volume and Open Issues over Time",
+			autosize: false,
+			width: 1200,
+			height: 800,
+			xaxis: {
+				title: "Week Number"
+			},
+			yaxis: {
+				title: "Lines of Code"
+			},
 			yaxis2: {
 				title: "Issues",
 				side: "right",
 				overlaying: "y"
-			}
-			};
+			},
+		};
 
 	var graphOptions = {layout: layout, 
 				filename: repo + " " + new Date(), 
