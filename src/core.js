9b4cec1d549151c7f10635e50cadf913ba65cb88
Greg Richardson <gregnr@uvic.ca>
2015-03-15 16:40:55 -0700

    Created server.js that will run an HTTP/Socket.IO server which serves both static HTML files and a web API over Socket.IO

---
+++ src/core.js
@@ -0,0 +1,105 @@
+var async = require("async");
+
+var analyser = require("./analyser")
+var issues = require("./issues");
+var sync = require("./sync");
+var graph = require("./graph");
+
+//Initialize globals
+var gIssues = [];
+var gCommits = [];
+var gTimeData = [];
+var gNumberIssuesData = [];
+var gDeltaVolumeTestsData = [];
+var gPlotlyGraphId;
+
+var gUsername;
+var gPassword;
+var gUrl;
+var gTestDirectory;
+
+
+var loadIssues = function(callback) {
+    
+    issues.get(
+
+        function (err, result) {         
+    
+            gIssues = result;
+            callback(err, result);
+            return;
+        }, 
+
+        gUsername,
+        gPassword,
+        gUrl
+    );
+
+};
+
+var loadCommits = function (callback) {
+
+    sync.loadCommits(gCommits, gUrl, callback);
+};
+
+var analyseRepo = function (callback) {
+
+    console.log("Beginning analysis");
+    
+    analyser.analyse(gTestDirectory, gIssues, gCommits, gUrl, function(data) {
+    
+        gTimeData = data.time;
+        gNumberIssuesData = data.numberIssues;
+        gDeltaVolumeTestsData = data.deltaVolumeTests;
+        
+        callback();
+    });
+};
+
+var generateGraph = function(callback) {
+
+    var repo_name = (/^.*\/([^\/]+)$/).exec(gUrl).slice(1);
+
+    var o_numberIssues = {
+	    x: gTimeData,
+	    y: gNumberIssuesData,
+	    type: "scatter",
+	    yaxis: "y2",
+	    name: "Number of Open Issues"
+    };
+
+    var o_deltaVolumeTests = {
+	    x: gTimeData,
+	    y: gDeltaVolumeTestsData,
+	    type: "scatter",
+	    name: "Change in Test Volume"
+    };
+
+    var data = [o_numberIssues, o_deltaVolumeTests];
+
+    graph.generateGraph(repo_name, data, function(response) {
+    
+        gPlotlyGraphId = response.plotlyGraphId;
+        
+        callback();
+    });
+};
+
+module.exports.analyseRepo = function (config, callback) {
+
+    gUsername = config.username;
+    gPassword = config.password;
+    gUrl = config.repoUrl;
+    gTestDirectory = config.testDirectory;
+
+    async.series([loadIssues, loadCommits, analyseRepo, generateGraph], function(err) {
+        
+        if (err) {
+            console.error(err);
+        }
+        
+        callback({
+            plotlyGraphId: gPlotlyGraphId
+        });
+    });
+};
